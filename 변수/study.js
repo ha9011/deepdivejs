a4f08647e28946dcc3d220526e70a1969fe02cc9
hadongwon <ha9011@naver.com>
2022-01-10 19:49:14 +0900

    변수

---
+++ 변수/study.js
@@ -0,0 +1,33 @@
+let userId = 1;
+let userName = "lee";
+// 변수는 하나의 값을 저장하기 위한 수단이다.
+let user = {
+  id: 1,
+  name: "lee",
+};
+
+let users = [
+  {
+    id: 2,
+    name: "lee",
+  },
+  {
+    id: 1,
+    name: "ha",
+  },
+];
+
+// 연산을 통해 새로운 값을 저장한다. 그리고 메모리 공간에 저장된다.
+// 재사용이 가능하다
+let result = 10 + 20;
+
+console.log(result);
+
+console.log(score);
+
+score = 90;
+var score;
+
+console.log(score);
+
+console.log(undefined);
